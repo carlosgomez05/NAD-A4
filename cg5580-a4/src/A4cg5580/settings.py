c34424f173d349e51492d72b4385fa5c9040fb44
carlosgomez05 <153135498+carlosgomez05@users.noreply.github.com>
2025-03-27 22:13:14 -0400

    Add forms, modify settings to add the template, add post with crispy format as instructions in <<Adding a Form to the Modal>> video

--- cg5580-a4/src/A4cg5580/settings.py
+++ cg5580-a4/src/A4cg5580/settings.py
@@ -37,14 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     # own apps
     'posts',
     'profiles',
-
     #3rd party apps
     'crispy_forms',
+    'crispy_bootstrap4',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
