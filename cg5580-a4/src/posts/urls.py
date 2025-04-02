9a8b671aca2d7ddefa495ba9a3c7f986fc3bfdc0
carlosgomez05 <153135498+carlosgomez05@users.noreply.github.com>
2025-04-02 11:27:22 -0400

    Update posts with ajax following video instructtions of <<Update Post with Ajax>> video. Fixed urls and corrected spelling

--- cg5580-a4/src/posts/urls.py
+++ cg5580-a4/src/posts/urls.py
@@ -14,10 +14,10 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', post_list_and_create, name='main-board'),
-    path('like-unlike', like_unlike_post, name='like-unlike'),
+    path('like-unlike/', like_unlike_post, name='like-unlike'),
     path('<pk>/', post_detail, name='post-detail'),
-    path('<pk>/update', update_post, name='post-update'),
-    path('<pk>/delete', update_post, name='post-delete'),
+    path('<pk>/update/', update_post, name='post-update'),
+    path('<pk>/delete/', delete_post, name='post-delete'),
 
     path('data/<int:num_posts>/', load_post_data_view, name='post-data'),
     path('<pk>/data/', post_detail_data_view, name='post-detail-data'),
